cc6251d47bf5d61f438756dc2f35ca87ad3e672f
rushjs1 <rushjsdev@gmail.com>
2022-05-07 14:21:24 -0400

    started spotify page

---
+++ src/components/icons/spotify/PlusIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <svg
+      class="w-4 h-4 fill-current text-gray-900"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      ></path>
+    </svg>
+  </div>
+</template>
