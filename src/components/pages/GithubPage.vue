846c73459c51a5a093b1301d62c497d885c4581b
rushjs1 <rushjsdev@gmail.com>
2022-05-12 00:33:17 -0400

    started file explorer - github page

--- src/components/pages/GithubPage.vue
+++ src/components/pages/GithubPage.vue
@@ -504,7 +504,7 @@
         </div>
         <!-- Commits Container -->
         <div
-          class="bg-blue-100/50 rounded-md rounded-b-none border border blue-200 border-b-none flex items-center justify-between px-4 py-4 mt-5"
+          class="bg-blue-100/50 rounded-md rounded-b-none border border-blue-100 border-b-none flex items-center justify-between px-4 py-4 mt-5"
         >
           <div class="flex items-center space-x-2">
             <img
@@ -565,7 +565,47 @@
         </div>
         <!-- Commits container end -->
         <!-- file explorer -->
-        <div>File Explorer</div>
+        <div
+          class="file-explorer rounded-md rounded-t-none border border-gray-200 text-gray-700 divide-y divide-gray-200"
+        >
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center space-x-3">
+              <FolderIcon />
+              <span class="hover:underline hover:text-blue-500">.github</span>
+            </div>
+            <div class="w-6/12">
+              <span class="hover:underline hover:text-blue-500"
+                >Update CONTRIBUTING.md</span
+              >
+              (<span class="text-blue-500 hover:underline">#7723</span>)
+            </div>
+            <div class="w-2/12 text-right">2 Months Ago</div>
+          </div>
+
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12 flex items-center space-x-3">
+              <FolderIcon />
+              <span>integrations</span>
+            </div>
+            <div class="w-6/12 truncate">
+              <span>
+                Support postcss config options in config file in the CLI (
+              </span>
+              <span class="text-blue-500 hover:underline">#8226</span>)
+            </div>
+            <div class="w-2/12 text-right">13 days ago</div>
+          </div>
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12">one</div>
+            <div class="w-6/12">two</div>
+            <div class="w-2/12 text-right">three</div>
+          </div>
+          <div class="flex justify-between px-4 py-2">
+            <div class="w-4/12">one</div>
+            <div class="w-6/12">two</div>
+            <div class="w-2/12 text-right">three</div>
+          </div>
+        </div>
         <!-- file explorer end -->
       </div>
       <div class="sidebar w-1/4">
@@ -577,3 +617,7 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import FolderIcon from "@/components/icons/github/FolderIcon.vue";
+</script>
