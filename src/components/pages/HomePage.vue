839948226a2cd96fc8a3e2c4ff46061ba4e55089
rushjs1 <rushjsdev@gmail.com>
2022-05-25 17:16:29 -0400

    change design on main page

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -2,27 +2,30 @@
   <div class="home-page">
     <div>
       <nav
-        class="nav p-6 flex justify-end space-x-6 text-white font-semibold bg-gray-800 border-b border-amber-500 fixed w-full shadow-lg"
+        class="nav p-6 flex justify-start items-center space-x-1 text-white font-semibold bg-slate-900 fixed w-full"
       >
         <RouterLink to="/">
           <TailwindLogo />
         </RouterLink>
+        <span class="font-bold text-lg font-mono">Tailwind Rebuilds</span>
       </nav>
-      <div class="bg-gray-600 h-screen text-center">
+      <div class="bg-slate-900 h-screen text-center">
         <div
-          class="title text-white flex justify-center items-center pt-22 text-5xl space-x-2 font-bold"
+          class="title text-white font-extrabold flex flex-col justify-center items-center pt-28 text-4xl tracking-tight space-x-2 font-bold px-4"
         >
-          <span> Tailwind Rebuilds </span>
-        </div>
-        <div class="mt-12 text-slate-200 text-xl px-8">
-          Rebuilding popular user interfaces using
-          <a
-            href="https://tailwindcss.com/"
-            class="underline hover:text-sky-400"
-            >tailwindcss</a
+          <span>
+            Rebuilding popular user interfaces using
+            <a
+              href="https://tailwindcss.com/"
+              class="hover:underline text-sky-400"
+              >tailwindcss</a
+            >
+          </span>
+          <span class="text-2xl mt-4 font-semibold"
+            >to practice and sharpen my knowledge of the framework.</span
           >
-          to practice and sharpen my knowledge of the framework.
         </div>
+
         <div
           class="sites-parent mt-20 px-20 grid grid-cols-2 sm:grid-cols-3 gap-x-20 md:gap-0"
         >
