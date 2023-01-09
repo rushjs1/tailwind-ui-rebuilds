d6efd056e6180d33b41a4a4a9fd4da447e0f8851
rushjs1 <rushjsdev@gmail.com>
2023-01-09 16:30:00 -0500

    refactor pt. 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import GithubView from '@/views/GithubView.vue'
-import HomeView from '@/views/HomeView.vue'
-import SpotifyView from '@/views/SpotifyView.vue'
-import PageNotFound from '@/components/pages/404/PageNotFound.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import GithubView from "@/views/GithubView.vue";
+import HomeView from "@/views/HomeView.vue";
+import SpotifyView from "@/views/SpotifyView.vue";
+import PageNotFound from "@/views/404/PageNotFound.vue";
 
 const routes = [
   {
-    path: '/:pathMatch(.*)*', 
-    name: 'page-not-found',
-    component: PageNotFound
+    path: "/:pathMatch(.*)*",
+    name: "page-not-found",
+    component: PageNotFound,
   },
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/Github-ui',
-    name:'github-ui',
-    component: GithubView
+    path: "/Github-ui",
+    name: "github-ui",
+    component: GithubView,
   },
   {
-    path: '/Spotify-ui',
-    name: 'spotify-ui',
-    component: SpotifyView
-  }
-]
+    path: "/Spotify-ui",
+    name: "spotify-ui",
+    component: SpotifyView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
